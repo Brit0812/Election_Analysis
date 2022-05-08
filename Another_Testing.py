c9ed6699c1d03923f37eabe22fd0016c11aa66ce
Brit0812 <britkura@outlook.com>
2022-05-08 15:01:27 -0400

    PyPoll Challenge Election Analysis.

--- Another_Testing.py
+++ Another_Testing.py
@@ -5,26 +5,26 @@
 #     x= x+1
 # numbers= [0, 1, 2, 3, 4, 5]
 # for num in numbers:
-#     print(num)
-# #same as above 
-# for num in range(5):
-#     print(num)
+# #     print(num)
+# # #same as above 
+# # for num in range(5):
+# #     print(num)
 
-import csv
-import os 
-dir(os)
+# import csv
+# import os 
+# dir(os)
 
-file_to_open= os.path.join("Resources","testing_file_stuff_CSV.csv")
-with open(file_to_open) as practice:
-    print(practice.read())
-file_to_practice= os.path.join("analysis","practicing_files.txt")
-open(file_to_practice, "w")
-files_practice= open(file_to_practice, "w")
-files_practice.write("Hello, im practicing")
+# file_to_open= os.path.join("Resources","testing_file_stuff_CSV.csv")
+# with open(file_to_open) as practice:
+#     print(practice.read())
+# file_to_practice= os.path.join("analysis","practicing_files.txt")
+# open(file_to_practice, "w")
+# files_practice= open(file_to_practice, "w")
+# files_practice.write("Hello, im practicing")
 
-with open(file_to_practice, 'w') as files_practice:
-   files_practice.write("hello world, how are you")
-   files_practice.write("\nCounties in the Elections\n -----------------------\nArapahoe\nDenver\nJefferson")
+# with open(file_to_practice, 'w') as files_practice:
+#    files_practice.write("hello world, how are you")
+#    files_practice.write("\nCounties in the Elections\n -----------------------\nArapahoe\nDenver\nJefferson")
 
 
 
@@ -93,3 +93,4 @@ with open(file_to_practice, 'w') as files_practice:
 #    txt_file.write(winning_candidate_summary)
 # # # rem, when commiting to git write "git add ." the periods important 
 # # #  otherwise youll get an error 
+
